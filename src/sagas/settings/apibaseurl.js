85c6433b8984891ed472e0571b3cc700452a93d0
sorozcov <42949349+sorozcov@users.noreply.github.com>
2020-06-01 23:54:58 -0600

    Demo  y api

--- src/sagas/settings/apibaseurl.js
+++ src/sagas/settings/apibaseurl.js
@@ -3,5 +3,5 @@
 /* -------------------------------------------------------------------------- */
 // Para probar esta app con react native se debe consultar
 let ip4 = '192.168.0.6';
-export default API_BASE_URL = `http://${api4}:8000/api/v1`;
+export default API_BASE_URL = `http://${ip4}:8000/api/v1`;
 //export default API_BASE_URL = 'http://10.0.0.9:8000/api/v1';
